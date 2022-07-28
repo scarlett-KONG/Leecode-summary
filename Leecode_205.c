a4e9df2a5be9f4bf0e840d79ab5d8dc9640ec024
Scarlett <1481710125@qq.com>
2022-07-29 06:35:43 +0800

    Update Leecode_205.c

--- Leecode_205.c
+++ Leecode_205.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
+/*First method. Easy to understand, but occupies too much memory and runs too slowly*/
 int *map(char *s){
     int *num = malloc((strlen(s))*sizeof(int));
     int i,j=0;
@@ -38,4 +38,35 @@ int main(){
         printf("false");
     }
     return 0;
+}
+
+/*Second method :  by taking use of ascii table, mapping each */
+bool isIsomorphic_2(char *s, char *t){
+    int len = strlen(s);
+    int sCharacter[128] = {0};
+    int tCharacter[128] = {0};//构建ASCII表
+
+    int s_counter = 1;
+    int t_counter = 1;
+
+    for(int i=0; i<len; i++){
+        if(sCharacter[s[i]] == 0){
+            sCharacter[s[i]] = s_counter;
+            s_counter++;//如果出现，则ASCII表对应位置赋值为1，每出现一次个数+1
+        }
+        if(sCharacter[s[i]] != 0){
+            s[i] = sCharacter[s[i]];
+        }
+        if(tCharacter[t[i]] == 0){
+            tCharacter[t[i]] = t_counter;
+            t_counter ++;
+        }
+        if(tCharacter[t[i]]!=0){
+            t[i] = tCharacter[t[i]];
+        }
+        if(s[i]!=t[i]){
+            return false;//对比每个位置的出现次数是否相同
+        }
+    }
+    return true;
 }
